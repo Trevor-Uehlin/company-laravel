377d0c91b077d096ea277a1c5b44cd36e943e658
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2022-04-13 14:30:02 -0700

    The 'x-app-layout' should be working with all pages.

---
+++ resources/views/projects.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout>
+    Here is a list of projects that I have worked on.
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,28 @@ use Illuminate\Support\Facades\Mail;
 |
 */
 
-Route::get('/home', function () {
-    return view('home');
+Route::middleware(['web'])->group(function(){
+
+    Route::get('/', function () {
+        return view('home');
+    });
+
+    Route::view('/projects', 'projects')->name('projects');
+    
+    // Route::get('/', function () {
+    //     return view('welcome');
+    // });
 });
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+
+Route::middleware(["auth"])->group(function(){
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+});
 
 
 require __DIR__.'/auth.php';
