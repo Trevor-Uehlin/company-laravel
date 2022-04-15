e16f1100e024fcda785227ff5d3ef4759fe7e8fb
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2022-04-15 10:10:44 -0700

    Added a dropdown for playground items to the navigation for desktop and mobile.

---
+++ resources/views/playground/item-1.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+
+    <h1>item 1</h1>
+
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,11 @@ Route::middleware(['web'])->group(function(){
     # Email Routes
     Route::resource('/contact', 'App\Http\Controllers\EmailController')->names(['index' => 'contact', 'store' => 'send']);
     Route::view('/email/success', 'email.success')->name('email/success');
+
+
+    # Playground Routes
+    Route::view('playground/item/1', 'playground.item-1')->name('play/item/1');
+    Route::view('playground/item/2', 'playground.item-2')->name('play/item/2');
 });
 
 
