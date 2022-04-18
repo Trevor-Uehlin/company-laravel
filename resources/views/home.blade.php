a01162c7242e1021c685cf3e11eff10e9693644c
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2022-04-18 13:05:24 -0700

    Woops

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,4 +4,14 @@
             {{ __('Home') }}
         </h2>
     </x-slot> --}}
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <p>This site is being built with Laravel and is a work in progress.  There is not a lot of content, but more is being added every day.</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
