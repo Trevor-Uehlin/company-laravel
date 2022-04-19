a4424714920b954c10d38d873a82c0125e445e60
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2022-04-19 11:27:13 -0700

    fix sitemap

--- resources/views/sitemap/index.blade.php
+++ resources/views/sitemap/index.blade.php
@@ -2,8 +2,8 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
         <loc>{{ url('/') }}</loc>
-        <lastmod>2022-04-16 19:00</lastmod>
-        <changefreq></changefreq>
+        <lastmod>2022-04-16 19:46 +00:00</lastmod>
+        <changefreq>weekly</changefreq>
         <priority>0.8</priority>
     </url>
 </urlset>
