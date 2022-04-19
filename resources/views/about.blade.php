2165576924db6647d08432d6f84951218bbede83
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2022-04-19 09:56:05 -0700

    Now using my custom sub-content-area component.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,35 +1,9 @@
 <x-app-layout>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    
-                    <h1>
-                        Here is a little information about me and my professional goals and programming experience.
-                    </h1>
-
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-    <div class="container">
-
-
-
-
-    </div>
+    <x-custom.sub-content-area>
+        <p>
+            This page will have a little information about my professional goals and programming experience.
+        </p>
+    </x-custom.sub-content-area>
 
 </x-app-layout>
