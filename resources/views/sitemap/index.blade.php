76f938363d96fd35454f9d14aa740bd5105894ce
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2022-04-19 13:49:40 -0700

    fix

--- resources/views/sitemap/index.blade.php
+++ resources/views/sitemap/index.blade.php
@@ -1,21 +1,21 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
-<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-        <loc>{{ url('/') }}</loc>
+<sitemapindex xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
+    <sitemap>
+        <loc>{{ ('/') }}</loc>
         <lastmod>{{$today}}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.8</priority>
-    </url>
-    <url>
-        <loc>{{ url('/about') }}</loc>
+    </sitemap>
+    <sitemap>
+        <loc>{{ ('/about') }}</loc>
         <lastmod>{{$today}}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.8</priority>
-    </url>
-    <url>
-        <loc>{{ url('/contact') }}</loc>
+    </sitemap>
+    <sitemap>
+        <loc>{{ ('/contact') }}</loc>
         <lastmod>{{$today}}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.8</priority>
-    </url>
-</urlset>
+    </sitemap>
+</sitemapindex>
