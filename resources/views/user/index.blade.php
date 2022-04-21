b5591c114973b3fdbdba0d341bcb2b2cf928f6ea
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2022-04-21 12:13:38 -0700

    css stuff...questionable.

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -3,6 +3,7 @@
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Users') }}
+            <a href="{{route("users.create")}}" class="float-right"><i class="fa fa-plus" style="font-size:40px;color:blue;"></i></a>
         </h2>
     </x-slot>
 
@@ -11,7 +12,6 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     There are currently {{count($users)}} users.
-                    <a href="{{route("users.create")}}"><i class="fa fa-plus" style="font-size:20px;color:blue;"></i>Create a New User</a>
                 </div>
             </div>
         </div>
@@ -33,7 +33,7 @@
                                     <a href="users/delete/{{$user->id}}" class="delete-button"><i class="fa fa-trash-o" style="font-size:40px;color:red;"></i></a>
                                 </div>
                             @endif
-                            <p>Created on {{Carbon\Carbon::parse($user->created_at)->format("l, F, Y")}}</p>
+                            <p>Created: {{Carbon\Carbon::parse($user->created_at)->format("D, F, Y")}}</p>
                             <p class="h4"><strong>{{$user->name}}</strong></p>
                             <p><strong>Email: </strong>{{$user->email}}</p>
                             <p><strong>User Type: </strong>{{$user->role->name}}</p>
